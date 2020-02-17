60837a735c7ba764d17e5f709417bebf9e4520ca
Fran <fjoteroherrero@infomm.local>
2020-02-17 17:55:38 +0100

    Version 1.0 rama master

---
+++ src/AplicacionOterHerFran.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class AplicacionOterHerFran extends Component {
+
+    render() {
+
+        return (
+
+            <h1> Hola </h1>
+
+        )
+
+    }
+
+}
+
+
+export default AplicacionOterHerFran;
