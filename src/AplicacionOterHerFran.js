2f813df7f2c16181b33c7e90d1bff6158ddb7fbf
Fran <fjoteroherrero@infomm.local>
2020-02-17 18:26:28 +0100

    Version 1.0 Ejercicio 1

--- src/AplicacionOterHerFran.js
+++ src/AplicacionOterHerFran.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react'
+import { Router, Link } from '@reach/router'
+import Distribucion1OterHerFran from './Distribucion1OterHerFran'
+import Distribucion2OterHerFran from './Distribucion2OterHerFran'
+import Distribucion3OterHerFran from './Distribucion3OterHerFran'
+import PruebasOterHerFran from './PruebasOterHerFran'
 
 class AplicacionOterHerFran extends Component {
 
@@ -6,7 +11,22 @@ class AplicacionOterHerFran extends Component {
 
         return (
 
-            <h1> Hola </h1>
+            <div>
+                <nav className="navFlexOterHerFran">
+                    <div className="navFlexOterHerFran__item1"> <Link to="/Distribucion1OterHerFran"> Distribucion 1 </Link> </div>
+                    <div className="navFlexOterHerFran__item2"> <Link to="/Distribucion2OterHerFran"> Distribucion 1 </Link> </div>
+                    <div className="navFlexOterHerFran__item3"> <Link to="/Distribucion3OterHerFran"> Distribucion 1 </Link> </div>
+                    <div className="navFlexOterHerFran__item4"> <Link to="/PruebasOterHerFran"> Distribucion 1 </Link> </div>
+                </nav>
+
+                <Router>
+                    <Distribucion1OterHerFran path="/Distribucion1OterHerFran"/>
+                    <Distribucion1OterHerFran path="/Distribucion2OterHerFran"/>
+                    <Distribucion1OterHerFran path="/Distribucion3OterHerFran"/>
+                    <Distribucion1OterHerFran path="/PruebasOterHerFran"/>
+                </Router>
+
+            </div>
 
         )
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CSS from './css/StyleOterHerFran.css'
 import AplicacionOterHerFran from './AplicacionOterHerFran';
 
 ReactDOM.render(<AplicacionOterHerFran />, document.getElementById('root'));
